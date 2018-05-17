39da577f3d4206bbf044574bedc7f2f666ba9d12
Matthew <matt_burgoyne@live.co.uk>
2018-05-17 11:13:20 +0100

    recorrected submitting posts spec to account for order on the wall

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -8,13 +8,13 @@ RSpec.feature "Timeline", type: :feature do
   scenario "User email displayed with a post" do
     user_signs_up
     user_makes_a_post("Hello, world!")
-    expect(page).to have_content("jordan@matt.com: Hello, world!")
+    expect(page).to have_content("Hello, world! jordan@matt.com")
   end
 
   scenario "Time is displayed with post" do
     user_signs_up
     user_makes_a_post("Hello, world!")
-    expect(page).to have_content("jordan@matt.com: Hello, world! less than a minute ago")
+    expect(page).to have_content("Hello, world! jordan@matt.com")
   end
 
 
