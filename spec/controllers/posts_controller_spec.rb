7743ca5afad9db7aa90ba6f567d27ad5924886ba
Daniel Miller <danielmiller5791@gmail.com>
2018-05-18 13:25:36 +0100

    votes logic working, need to get votes/likes on the view to pass the test

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -17,15 +17,18 @@ RSpec.describe PostsController, type: :controller do
       PostsController.any_instance.stub(:current_user).and_return(User.first)
       save_sample_posts(user)
       get :index
-
       expect(response.body).to have_text(/3rd post entered[\S|\s]+2nd post entered[\S|\s]+1st post entered/)
     end
 
     it 'allows the user to like a post and show the number of times a post has been liked' do
       user = User.create(email: 'example@gmail.com', password: 123456)
+      user.save
       PostsController.any_instance.stub(:current_user).and_return(User.first)
       liked_post = user.posts.build(message: 'post to be liked', id: 1)
       liked_post.save
+      liked_post.liked_by user
+      get :index
+      expect(response.body).to have_text(/post to be liked[\s|\S]+likes: 1/)
     end
   end
 end
