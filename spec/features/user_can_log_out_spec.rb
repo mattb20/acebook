b521c528bfa2ddf64bf0ecba826be5e8707216a9
Matthew <matt_burgoyne@live.co.uk>
2018-05-18 09:01:48 +0100

    refactored logout feature test to include helper method

--- spec/features/user_can_log_out_spec.rb
+++ spec/features/user_can_log_out_spec.rb
@@ -1,12 +1,8 @@
 require 'rails_helper'
-
+require './helpers'
 RSpec.feature "Logout", type: :feature do
   scenario "User can logout" do
-    visit "/users/sign_up"
-    fill_in "user_email", with: "example@example.com"
-    fill_in "user_password", with: "ajdpwomakwmnd29q0jediaw"
-    fill_in "user_password_confirmation", with: "ajdpwomakwmnd29q0jediaw"
-    click_button "Sign up"
+    user_signs_up
     page.find_by_id('log_out').click
     expect(page).to have_content("Signed out successfully")
   end
